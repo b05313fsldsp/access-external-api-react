a3bdfce039f3978ca421c35655f4de36583f179f
david <rd02navc2@gmail.com>
2021-09-16 11:05:31 +0800

    a. App.js - CSS - chevron - Container - component JSX added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import React, { useEffect, useState } from "react";
 
 const kusUrl = "http://10.3.1.194:8081/api/tutorials";
 
+// component - 箭頭函式（arrow function）- 建立一個名為 Counter 的 React 組件
+const Counter = () => (
+    <div className="container">
+      <div className="chevron chevron-up" />
+      <div className="number">256</div>
+      <div className="chevron chevron-down" />
+    </div>
+);
+
+
 function App() {
 
   const word = 'React';
@@ -29,20 +39,31 @@ function App() {
   console.log(userData.SN);
 
   return (
-    <div className="App">
+    <div className="App">   
       <header className="App-header">
-        <h2>TQS Data</h2>
-        <h1>hello, {word}</h1>
-        // JSX 中可以在 {} 內放入表達式（expression）
-        <h1>現在 {deviceName} 的售價是 {currentPrice * discount}</h1>
 
-
-        <h2>{userData.SN}</h2>
       </header>
+
+      <div className="Container">
+        <h2>TQS Data</h2>
+        <h2>{userData.SN}</h2>
+      </div>
+        
       <div className="user-container">
         <h5 className="info-item">{userData.SN}</h5>
         <h5 className="info-item">{userData.SPN1761}</h5>
       </div>
+
+      <div className="Container">
+        <h1>hello, {word}</h1>
+        // JSX 中可以在 {} 內放入表達式（expression）
+        <h1>現在 {deviceName} 的售價是 {currentPrice * discount}</h1>
+        <Counter />
+        <Counter />
+        <Counter />
+      </div>
+
+
     </div>
   );
 }
